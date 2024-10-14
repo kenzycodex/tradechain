dac0822c032930c8a6c34941ae009d64ade88c1b
Kenzy Codex 😎❣ <kenzycodex@gmail.com>
2024-10-14 09:37:42 +0100

    Update page.tsx

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Sign Up - Open PRO",
-  description: "Page description",
+  title: "Sign Up - TradeChain",
+  description: "Trade Smarter, Thrive Faster",
 };
 
 import Link from "next/link";
